0e1e4615bc5f828662e7e2e453733937e02a2ee6
Deepak <dpkpr@outlook.com>
2020-03-11 13:05:36 +0530

    Added more experience.

--- src/app/modules/material/components/works/works.component.ts
+++ src/app/modules/material/components/works/works.component.ts
@@ -15,8 +15,8 @@ export class WorksComponent implements OnInit {
       {
         'domain': 'Networking, Network security, DevOps and Application development',
         'name': 'PrimeSoft Solutions, Inc.',
-        'position': 'Full Stack Developer',
-        'yom': '21 Months (1st June, 2016 to Present)',
+        'position': 'Associate Software Engineer',
+        'yom': '2 Years 2 Months (1st June, 2016 to 31st July, 2018)',
         'projects': [
           {
             'name': 'Google API integration with Python',
@@ -45,6 +45,36 @@ export class WorksComponent implements OnInit {
             'responsibility': 'Development of an android app',
             'role': 'Mobile Development',
             'tools': 'Android studio'
+          }
+        ]
+      }, 
+      {
+        'domain': 'Application development',
+        'name': 'o9 Solutions (Contracted by MIPL)',
+        'position': 'Software Engineer - Python',
+        'yom': '6 Months (18th Feb, 2019 to 31st July, 2019)',
+        'projects': [
+          {
+            'name': 'Tenant Extractor',
+            'discription': 'Data extraction from the platform tenants json file.',
+            'responsibility': 'Complete end to end development of this system app. Data mining and extraction of important data from the hugh data present in the tenant config file and present this data as sqlite file, csv files and xlsx files.',
+            'role': 'Developer',
+            'tools': 'Python, Data mining, sqlite, pandas'
+          }
+        ]
+      },
+      {
+        'domain': 'Application development',
+        'name': 'o9 Solutions',
+        'position': 'Senior Software engineer',
+        'yom': '8 Months (1st Aug, 2019 to 20th March, 2020)',
+        'projects': [
+          {
+            'name': 'Tenant Builder',
+            'discription': 'Build tenant using APIs calls using the data extracted from Tenant Extractor app.',
+            'responsibility': 'Complete end to end development of this system app. Create DDL queries using the data extracted from the Tenant Extractor app and pass this DDLs using an API call.',
+            'role': 'Developer',
+            'tools': 'Python, sqlite'
           }
         ]
       }
